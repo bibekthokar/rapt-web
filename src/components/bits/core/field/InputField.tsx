9c28ba0eccc4ddfaf648cfd38b63b6640d1c9d31
govinda <govindaranamagar44@gmail.com>
2021-01-31 17:26:38 +0545

    login form

--- src/components/bits/core/field/InputField.tsx
+++ src/components/bits/core/field/InputField.tsx
@@ -2,14 +2,13 @@ import React, { useState } from 'react';
 import { withTheme } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
 import { Avatar, Grid, Link, TextFieldProps, Typography } from '@material-ui/core';
-
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
 
 }));
-const InputField = ({ fieldName }) => {
+const InputField = ({ fieldName }: any) => {
     const classes = useStyles();
     const [field, setField] = useState('');
     console.log(fieldName, 'asdddddddddddddddd')
