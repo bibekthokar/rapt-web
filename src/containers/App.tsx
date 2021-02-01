372ce3692e81d4857908141d692d0cf540fc36ba
govinda <govindaranamagar44@gmail.com>
2021-02-01 11:52:41 +0545

    login page complet and  project page start

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,12 +1,13 @@
-import { Button } from '@material-ui/core';
 import Test from '../components/Test';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Index from '../components/page/login';
+import LoginIndex from '../components/page/login';
+import ProjectDashboard from '../components/page/dashboard/project';
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/login" component={Index} />
+        <Route exact path="/project" component={ProjectDashboard} />
+        <Route exact path="/login" component={LoginIndex} />
       </div>
     </Router>
   );
