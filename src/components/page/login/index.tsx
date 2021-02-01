8050635d016b137c690a5a37417702076243abcc
govinda <govindaranamagar44@gmail.com>
2021-02-01 11:28:39 +0545

    login

--- src/components/page/login/index.tsx
+++ src/components/page/login/index.tsx
@@ -3,30 +3,44 @@ import { withTheme } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
 import { Avatar, Grid, Link, Typography } from '@material-ui/core';
 import InputField from '../../bits/core/field/InputField';
-
+import EmailOutlinedIcon from '@material-ui/icons/EmailOutlined';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import ButtonField from '../../bits/core/button/ButtonField';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 interface Props { }
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
     loginForm: {
-        padding: "35px",
-        height: '100vh'
+        padding: "3.5rem",
+        height: '100vh',
+        fontFamily: 'Roboto',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+
     },
     form: {
-        marginTop: '125px',
-        paddingLeft: '40px',
+        marginTop: '13.9%',
+        padding: '0 4rem',
         '& h2': {
             fontSize: '3rem',
             fontWeight: 'bold',
             color: theme.palette.primary.main,
-            // color: '#1e2447',
+            [theme.breakpoints.down('md')]: {
+                fontSize: '2.5rem',
+            },
         },
         '& p': {
-            marginTop: '1.3%',
+            marginTop: '1.2rem',
             fontSize: '14px',
-            letterSpacing: '1.86',
+            letterSpacing: '.3px',
             color: theme.palette.primary.main,
+            [theme.breakpoints.down('md')]: {
+                letterSpacing: '0.3px',
+            },
 
         }
     },
@@ -41,28 +55,53 @@ const useStyles = makeStyles((theme) => ({
         }
     },
     field: {
-        marginTop: '4.1%'
+        marginTop: '3.7rem'
+    },
+    button: {
+        marginTop: '3rem'
+    },
+    copyright: {
+        fontSize: '11px',
+        fontFamily: 'Roboto',
+        color: 'rgb(30 , 36 , 71 , .5)',
+    },
+    loginWrap: {
+        display: 'block'
     }
 }));
 const Index = (props: Props) => {
     const classes = useStyles();
-    const [email, setEmail] = useState('');
     return (
         <section className={classes.root}>
             <Grid container spacing={0}>
                 <Grid item xs={5} className={classes.loginForm}>
-                    <Link className={classes.logo}>
-                        <Avatar alt="Remy Sharp" src="/asset/img/logo.svg" />
-                    </Link>
-                    <div className={classes.form}>
-                        <Typography variant="h2" >
-                            Login to your account
+                    <div className={classes.loginWrap}>
+                        <Link className={classes.logo}>
+                            <Avatar alt="Remy Sharp" src="/asset/img/logo.svg" />
+                        </Link>
+                        <div className={classes.form}>
+                            <Typography variant="h2" >
+                                Login to your account
                             </Typography>
-                        <p>Login Met Monitoring to manage, view and your projects</p>
-                        <div className={classes.field}>
-                            <InputField fieldName="email" />
+                            <p>Login Met Monitoring to manage, view and your projects</p>
+                            <div className={classes.field}>
+                                <InputField fieldName="Email"
+                                    type="text" icon={<EmailOutlinedIcon />} placeholder="Your email address here"
+                                    error="The input is not a valid email!" />
+                                <InputField fieldName="Password"
+                                    type="password" icon={<LockOutlinedIcon />}
+                                    visibleEye={<VisibilityIcon />}
+                                    hideEye={<VisibilityOffIcon />}
+                                    placeholder="Your password here"
+                                    error="Bad Email or password!" />
+                                <div className={classes.button}>
+                                    <ButtonField nameValue="Login" />
+                                </div>
+                            </div>
+
                         </div>
                     </div>
+                    <p className={classes.copyright}>Met Monitoring ©2021 </p>
                 </Grid>
                 <Grid item xs={7}>
                     <div className={classes.image}>
