4237457cd5325ff843404b5255dc76b237b5bb2f
bibekthokar <bibekthokar1@gmail.com>
2021-02-01 10:59:39 +0545

    type added in field

--- src/components/bits/core/field/InputField.tsx
+++ src/components/bits/core/field/InputField.tsx
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
 import { withTheme } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
-import { Avatar, Grid, Link, TextFieldProps, Typography } from '@material-ui/core';
+import {
+  Avatar,
+  Grid,
+  Link,
+  TextFieldProps,
+  Typography,
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-
+  root: {
+    flexGrow: 1,
+  },
 }));
-const InputField = ({ fieldName }) => {
-    const classes = useStyles();
-    const [field, setField] = useState('');
-    console.log(fieldName, 'asdddddddddddddddd')
-    return (
-        <fieldset>
-            <label htmlFor="email">{fieldName}</label>
-            <input type="text" name="email" id="email" value={field} />
-        </fieldset>
-    )
+const InputField = ({ fieldName }: any) => {
+  const classes = useStyles();
+  const [field, setField] = useState('');
+  console.log(fieldName, 'asdddddddddddddddd');
+  return (
+    <fieldset>
+      <label htmlFor="email">{fieldName}</label>
+      <input type="text" name="email" id="email" value={field} />
+    </fieldset>
+  );
 };
 
 export default InputField;
